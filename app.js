831707dce0f9b494690d16e92ed1258d25152d88
Aaron Hawkey <aaronhawkey48@gmail.com>
2018-08-31 00:53:07 -0500

    Added queue model.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const socket = require('socket.io');
 const mg = require('mongoose');
+const Queue = require('./models/Queue.js');
+const Secrets = require('./secrets.json');
 
-//mg.connect('mongodb://admin:5rdxdr5#1@ds137812.mlab.com:37812/matchmaking');
+mg.connect('mongodb://'+ Secrets.database.username +':'+ Secrets.database.password +'@ds137812.mlab.com:37812/matchmaking', {
+    useNewUrlParser: true
+});
 
-const app = express()
+const app = express();
 
 app.set('view engine', 'ejs');
 
@@ -25,6 +29,31 @@ io.on('connection', (socket)=>{
     console.log(socket.id);
     
     socket.on('queue-up', (data)=>{
-        console.log(data.user_id);
+        
+        // Querying user_id to see if already in Queue
+        result = Queue.findOne({user_id: data.user_id}, (err, queue)=>{
+            if(err){
+                console.log(err);
+            }
+        });
+        
+        // Checks to see if query came back.
+        if(!result){
+            var newQueuer = new Queue({
+                _id: new mg.Types.ObjectId(),
+                user_id: data.user_id
+            });
+    
+            newQueuer.save()
+                .then(result =>{
+                    console.log(result);
+                })
+                .catch(err =>{
+                    console.log(err);
+                })
+        }else{
+            // User already exists.
+            console.log('User already exists');
+        }
     });
 });
